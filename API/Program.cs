b4b0d105258ad25f0b6b7ad75d7e2e15a3010ba6
Abhi <abhiram.patankar@niceactimize.com>
2025-06-20 11:10:54 +0530

    Created Database using Entity framework first migrations

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,7 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//--Adding DbContext service
+// --providing options
+// --using UseSqlite as Options
+// --building connection string and naming it
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8479526d62cb03849a3da2c26b72997c273ba2be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f906218c11e53aa950ec98eb0e41b2154fdb4671")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
