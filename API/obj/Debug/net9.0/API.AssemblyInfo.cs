c971838281b0a488c0e2c961cc354be729cd3c29
Abhi <abhiram.patankar@niceactimize.com>
2025-06-20 12:34:53 +0530

    Build walking skeleton -  API

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f906218c11e53aa950ec98eb0e41b2154fdb4671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b0d105258ad25f0b6b7ad75d7e2e15a3010ba6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
